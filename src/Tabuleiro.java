939c81ace5cfd7184fbe122b6d6f6e3b99357ac3
Thiago Klein de Angelis <thiagokda1@gmail.com>
2015-10-13 22:46:53 -0300

    Setting up configurations

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -4,7 +4,8 @@ public class Tabuleiro {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Teste");
+		System.out.println("Teste First Commit");
+		System.out.println("Testing clone.");
 	}
 
 }
